9a63f7461dfe5f69860fea17a17fd05e8d9e5f75
Nathan Corry <n.corry@2016.ljmu.ac.uk>
2017-10-19 17:06:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,9 @@
 def fizzbuzz(int)
-     if int % 15 == 0
-         "FizzBuzz"
-     elsif i % 3 == 0
-         "Fizz"
-     elsif i % 5 == 0
-         "Buzz"
-
-     end
-  end
+   if int % 15 == 0
+      puts "FizzBuzz"
+   elsif int % 3 == 0
+      puts "Fizz"
+   elsif int % 5 == 0
+      puts "Buzz"
+   
+end
