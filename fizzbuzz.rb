f1585b5e196f9fdd0fa8ac15bbde01b54ad2229a
Nathan Corry <n.corry@2016.ljmu.ac.uk>
2017-10-19 17:08:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,14 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  else
+
+  elsif condition
+    
      int % 5 == 0 # if the number int is divisible by 3
      "Buzz" # Go fizzbuzz
-  else
+
+  elsif 
+    
     int % 15 == 0  # if the number int is divisible by 3
     "Buzz" # Go fizzbuzz
     end
